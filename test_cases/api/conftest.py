3947f135017882c649c32b9ded425348c516c005
Christopher <christopher0906@hotmail.com>
2024-08-17 21:07:55 +0800

    fix merge issues

--- test_cases/api/conftest.py
+++ test_cases/api/conftest.py
@@ -13,16 +13,16 @@ def api_test_data() -> list:
     """
     path = os.getcwd()
     with open(f"{path}/config/env.json", "r") as file:
-        # config = json.load(file)
-        # url = config["UAT"]["url"]
-        # response = requests.get(url, verify=False)
-        # soup = BeautifulSoup(response.content, "html.parser")
-        #
-        # # find all tags of image
-        # img_tags = soup.find_all("img")
+        config = json.load(file)
+        url = config["UAT"]["url"]
+        response = requests.get(url, verify=False)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # find all tags of image
+        img_tags = soup.find_all("img")
         urls = []
-        # for img in img_tags:
-        #     img_url = img.get("src")
-        #     img_url = urljoin(url, img_url)
-        #     urls.append(img_url)
+        for img in img_tags:
+            img_url = img.get("src")
+            img_url = urljoin(url, img_url)
+            urls.append(img_url)
         return urls
--- test_cases/api/test_mCDN_setting.py
+++ test_cases/api/test_mCDN_setting.py
@@ -26,12 +26,11 @@ class TestCacheRate:
     @pytest.mark.cache_hit
     @pytest.mark.parametrize("url", urls)
     def test_cache_rate_hit(self, url: str):
-        # """
-        # This scenario only verify the hit rate of get image files
-        # :param url: image url
-        # :return: None
-        # """
-        # res_img = requests.get(url, verify=False)
-        # cache_status = res_img.headers.get('X-Mly-Cache', '')
-        # assert "HIT" in cache_status
-        pass
+        """
+        This scenario only verify the hit rate of get image files
+        :param url: image url
+        :return: None
+        """
+        res_img = requests.get(url, verify=False)
+        cache_status = res_img.headers.get('X-Mly-Cache', '')
+        assert "HIT" in cache_status
